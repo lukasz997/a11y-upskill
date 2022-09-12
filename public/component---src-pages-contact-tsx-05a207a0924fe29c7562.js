55e02f5c1a3b51b089d97d8e0b6fbb4f360fcb32
Lukasz Radecki <lukaszradecki404@gmail.com>
2022-09-12 02:06:53 +0200

    removed public

--- public/component---src-pages-contact-tsx-05a207a0924fe29c7562.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunka11y_upskill=self.webpackChunka11y_upskill||[]).push([[650],{6865:function(n,t,u){u.r(t);var l=u(4382);t.default=function(){return(0,l.tZ)("main",{style:{padding:300}},(0,l.tZ)("h1",null,"Contact"))}}}]);
-//# sourceMappingURL=component---src-pages-contact-tsx-05a207a0924fe29c7562.js.map
